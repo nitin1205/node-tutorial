970044811f9775b537edafead20abb6480ac7db6
Nitin Singh <vortechcorp@gmail.com>
2023-01-15 19:54:40 +0530

    created separate dir for routing and handled request using express router

--- server.js
+++ server.js
@@ -211,19 +211,40 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // middleware to serev static file
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
+
+// imported routes
+app.use('/', require('./routes/root'));
+
+app.use('/subdir', require('./routes/subdir'));
+
+app.use('/employees', require('./routes/api/employees'));
+
 
 // route handlers
-app.get('/', (req, res) => {
-    res.send('index');
-});
+// app.get('/', (req, res) => {
+//     res.send('index');
+// });
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/*', (req, res) => {
-    res.status(400).send('404 not found')
+// app.get('/*', (req, res) => {
+//     res.status(400).send('404 not found')
+// });
+
+
+app.all('*', (req, res) => {
+    res.status(400)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not found' });
+    } else {
+        res.type('txt').send('404 not found');
+    }
 });
 
 app.use(errorHandler);
