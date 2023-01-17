402030bd6006a534faf89fae6f2b33256c695d62
Nitin Singh <vortechcorp@gmail.com>
2023-01-17 22:05:21 +0530

    created user and employee model and used user model to register new user

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,31 +1,42 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-};
+// const usersDB = {
+//     users: require('../model/users.json'),
+//     setUsers: function (data) { this.users = data }
+// };
 
-const fsPromises = require('fs').promises;
-const path = require('path');
+// const fsPromises = require('fs').promises;
+// const path = require('path');
 const bcrypt = require('bcrypt');
+
+const User = require('../model/User');
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required' });
     // check for duplicate username
-    const duplicate = usersDB.users.find(person => person.username === user);
+    // const duplicate = usersDB.users.find(person => person.username === user);
+    const duplicate = await User.findOne({ username: user }).exec();
     if (duplicate) return res.sendStatus(409); //conflict
     try {
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        const newUser = { 
-            'username': user,
-            "roles": {"Users": 2001},
-            'password': hashedPwd
-        };
-        usersDB.setUsers([...usersDB.users, newUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        console.log(usersDB.users);
+        // const newUser = { 
+        //     'username': user,
+        //     "roles": {"Users": 2001},
+        //     'password': hashedPwd
+        // };
+        // usersDB.setUsers([...usersDB.users, newUser]);
+        // await fsPromises.writeFile(
+        //     path.join(__dirname, '..', 'model', 'users.json'),
+        //     JSON.stringify(usersDB.users)
+        // );
+        // console.log(usersDB.users);
+
+        // create and store new user
+        const result = await User.create({
+            "username": user,
+            "password": hashedPwd
+        });
+
+        console.log(result);
         res.status(201).json({ 'success': `New user ${user} created.`})
     } catch (err) {
         res.status(500).json({ 'message': err.message });
